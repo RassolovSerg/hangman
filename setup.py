3c7128404abf364148b94d3a464e00a57d8597f1
shkurak <shkurak@phystech.edu>
2017-09-28 02:27:23 +0300

    Add travis

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.7
+
+"""Setup script."""
+
+from setuptools import setup
+
+setup(
+    name="hangman",
+    version="0.0.0",
+    author="Ivan Shkurak",
+    author_email="shkurakivan@gmail.com",
+    url="https://github.com/shkurak/hangman",
+    license="MIT",
+    packages=[
+        "hangman",
+    ],
+    install_requires=[
+    ],
+    setup_requires=[
+        "pytest-runner",
+        "pytest-pylint",
+        "pytest-pycodestyle",
+        "pytest-pep257",
+        "pytest-cov",
+    ],
+    tests_require=[
+        "pytest",
+        "pylint",
+        "pycodestyle",
+        "pep257",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
