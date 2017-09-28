0ded78ab70eba7110c86ffc08a00d22e602e6a02
shkurak <shkurak@phystech.edu>
2017-09-29 01:51:23 +0300

    Add test

--- tests/test_print_word.py
+++ tests/test_print_word.py
@@ -1,9 +1,10 @@
-from hangman import reader
+from hangman import print_word
+import sys
 
-
-def test_check():
-    name = 'kakashka.dat'
-    with open(name, 'w') as kakashka:
-        kakashka.write('kek')
-
-    assert reader(name) == ['kek']
+def test_print_word():
+    sys.stdout = open('file', 'w')
+    print_word('nano', set(['n','o']))
+    sys.stdout.close()
+    with open('file', 'r') as ff:
+        data = ff.read()
+    assert data == 'The word: n*no\n\n'
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,9 +1,9 @@
 from hangman import reader
 
 
-def test_check():
+def test_reader():
     name = 'kakashka.dat'
     with open(name, 'w') as kakashka:
-        kakashka.write('kek')
+        kakashka.write('kek' + '\n')
 
     assert reader(name) == ['kek']
