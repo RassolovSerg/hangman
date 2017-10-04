e4103171b3a776e728c75012845b99f8c8fb8984
shkurak <shkurak@phystech.edu>
2017-10-04 14:58:58 +0300

    Make OOP oriented

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -12,36 +12,8 @@ def print_word(word, guesses):
     print
 
 
-def check(word, guesses):
-    answer = True
-    for i in word:
-        if i not in guesses:
-            answer = False
-    return answer
-
-
 def letter_input():
     return raw_input()
-
-
-def iteration(self, (word, mistakes, missed, guesses)):
-    boolean = True
-    letter = letter_input()
-    guesses.add(letter)
-    if letter in word:
-        print 'Hit!\n'
-        if check(word, guesses):
-            print_word(word, guesses)
-            print 'You won!'
-            boolean = False
-    else:
-        missed += 1
-        print 'Missed, mistake', missed, 'out of', mistakes, '\n'
-        if missed >= mistakes:
-            print_word(word, guesses)
-            print 'You lost!'
-            boolean = False
-    return boolean, missed, guesses
 
 
 class Game(object):
@@ -54,12 +26,30 @@ class Game(object):
         self.guesses = set()
         self.boolean = True
 
+    def iteration(self, (word, mistakes, missed, guesses)):
+        letter = letter_input()
+        guesses.add(letter)
+        if letter in word:
+            print 'Hit!\n'
+            if check(word, guesses):
+                print_word(word, guesses)
+                print 'You won!'
+                self.boolean = False
+        else:
+            missed += 1
+            print 'Missed, mistake', missed, 'out of', mistakes, '\n'
+            if missed >= mistakes:
+                print_word(word, guesses)
+                print 'You lost!'
+                self.boolean = False
+        return missed, guesses
+
     def play(self):
         while self.boolean:
             print_word(self.word, self.guesses)
             print 'Guess a letter:'
-            arguments = self.word, self.mistakes, self.missed, self.guesses
-            self.boolean, self.missed, self.guesses = self.iteration(arguments)
+            arguments = (self.word, self.mistakes, self.missed, self.guesses)
+            self.missed, self.guesses = self.iteration(arguments)
 
 
 def main():
